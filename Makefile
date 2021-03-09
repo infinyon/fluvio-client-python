5b64f3cd3310094922e43e6bd0aaaaa45ddf5cb4
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-03-09 14:48:44 -0800

    ugh

--- Makefile
+++ Makefile
@@ -13,15 +13,22 @@ venv-pip: venv
 build-wheel: venv-pip
 	$(PYTHON) setup.py bdist_wheel
 
+install-wheel: build-wheel
+	rm -r ./venv/lib/python3.9/site-packages/fluvio*
+	$(PIP) install --upgrade --no-index --pre --find-links=dist/ fluvio
+
 build-dev: venv-pip
 	#$(PYTHON) setup.py develop
 	$(PYTHON) setup.py install
 
-test: build-dev
-	fluvio topic create my-topic-iterator || true
-	fluvio topic create my-topic-while || true
-	fluvio topic create my-topic-produce || true
-	$(PYTHON) setup.py test
+test: install-wheel
+	#fluvio topic create my-topic-iterator || true
+	#fluvio topic create my-topic-while || true
+	#fluvio topic create my-topic-produce || true
+	cd tests && ../venv/bin/python -m unittest
+	fluvio topic delete my-topic-iterator || true
+	fluvio topic delete my-topic-while || true
+	fluvio topic delete my-topic-produce || true
 
 ci-build: venv-pip
 	CIBW_SKIP="cp27-*" $(PYTHON) -m cibuildwheel --platform linux --output-dir wheelhouse
--- setup.py
+++ setup.py
@@ -1,18 +1,45 @@
 from setuptools import setup, find_namespace_packages
 from setuptools_rust import Binding, RustExtension, Strip
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 
 setup(
     name='fluvio',
     version="0.0.1",
-    long_description=readme(),
+    packages=["fluvio"],
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author = "Fluvio Contributors",
+    description='Python client library for Fluvio',
+    python_requires='>=3.6',
+    url='https://www.fluvio.io/',
+    keywords=['fluvio', 'streaming', 'stream'],
+    license='APACHE',
     author_email = "team@fluvio.io",
-    packages=find_namespace_packages(include=['fluvio.*']),
-    zip_safe=False,
+    setup_requires=['wheel'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/infinyon/fluvio-client-python/issues',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/infinyon/fluvio-client-python',
+    },
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[  # Optional
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
     rust_extensions=[RustExtension("fluvio.fluvio_rust", path="Cargo.toml", binding=Binding.RustCPython, debug=False)],
+
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
 )
