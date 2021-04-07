441ccc56141e3e3889b118b5cdaeb003e316b39f
simlay <simlay@users.noreply.github.com>
2021-04-07 19:56:55 -0400

    Added cloud test ci (#16)
    * Added cloud ci

--- tests/test_fluvio_python.py
+++ tests/test_fluvio_python.py
@@ -1,8 +1,50 @@
 from fluvio import (Fluvio, FluviorError, Offset)
 import unittest
+import platform
+
+
+def topic_prefix(topic_suffix):
+    return (
+        '%s-%s-%s-%s' % (
+            platform.system().lower(),
+            platform.python_version_tuple()[0],
+            platform.python_version_tuple()[1],
+            topic_suffix,
+        )
+    )
+
+
+def create_topic(topic):
+    import subprocess
+    subprocess.run("fluvio topic create %s" % topic, shell=True)
+
+
+def delete_topic(topic):
+    import subprocess
+    subprocess.run("fluvio topic delete %s" % topic, shell=True)
 
 
 class TestFluvioMethods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        for i in [
+            'my-topic-produce',
+            'my-topic-iterator',
+            'my-topic-key-value-iterator',
+            'my-topic-batch-producer'
+         ]:
+            create_topic(topic_prefix(i))
+
+    @classmethod
+    def tearDownClass(cls):
+        for i in [
+            'my-topic-produce',
+            'my-topic-iterator',
+            'my-topic-key-value-iterator',
+            'my-topic-batch-producer'
+        ]:
+            delete_topic(topic_prefix(i))
+
     def test_connect(self):
         # A very simple test
         Fluvio.connect()
@@ -10,31 +52,35 @@ class TestFluvioMethods(unittest.TestCase):
     def test_produce(self):
         fluvio = Fluvio.connect()
 
-        producer = fluvio.topic_producer('my-topic-produce')
+        producer = fluvio.topic_producer(topic_prefix('my-topic-produce'))
         for i in range(10):
             producer.send_record_string("FOOBAR %s " % i, 0)
 
     def test_produce_on_uncreated_topic(self):
         fluvio = Fluvio.connect()
+        topic = topic_prefix('a-topic-that-does-not-exist')
 
-        producer = fluvio.topic_producer('a-topic-that-does-not-exist')
+        producer = fluvio.topic_producer(topic)
         try:
             producer.send_record_string("THIS SHOULD FAIL", 0)
         except FluviorError as e:
 
             self.assertEqual(
                 e.args,
-                ('Partition not found: a-topic-that-does-not-exist-0',)
+                (
+                    'Partition not found: %s-0' % topic,
+                )
             )
             print('ERROR: %s' % e)
 
     def test_consume_with_iterator(self):
         fluvio = Fluvio.connect()
-        producer = fluvio.topic_producer('my-topic-iterator')
+        topic = topic_prefix('my-topic-iterator')
+        producer = fluvio.topic_producer(topic)
         for i in range(10):
             producer.send_record_string("record-%s" % i, 0)
 
-        consumer = fluvio.partition_consumer('my-topic-iterator', 0)
+        consumer = fluvio.partition_consumer(topic, 0)
         count = 0
         for i in consumer.stream(Offset.beginning()):
             print("THIS IS IN AN ITERATOR! %s" % i.value())
@@ -48,11 +94,12 @@ class TestFluvioMethods(unittest.TestCase):
 
     def test_key_value(self):
         fluvio = Fluvio.connect()
-        producer = fluvio.topic_producer('my-topic-key-value-iterator')
+        topic = topic_prefix('my-topic-key-value-iterator')
+        producer = fluvio.topic_producer(topic)
         for i in range(10):
             producer.send("foo".encode(), ("record-%s" % i).encode())
 
-        consumer = fluvio.partition_consumer('my-topic-key-value-iterator', 0)
+        consumer = fluvio.partition_consumer(topic, 0)
         count = 0
         for i in consumer.stream(Offset.beginning()):
             print(
@@ -74,7 +121,8 @@ class TestFluvioMethods(unittest.TestCase):
 
     def test_batch_produce(self):
         fluvio = Fluvio.connect()
-        producer = fluvio.topic_producer('my-topic-batch-producer')
+        topic = topic_prefix('my-topic-batch-producer')
+        producer = fluvio.topic_producer(topic)
 
         records = []
         for i in range(10):
@@ -83,7 +131,7 @@ class TestFluvioMethods(unittest.TestCase):
 
         producer.send_all(records)
 
-        consumer = fluvio.partition_consumer('my-topic-batch-producer', 0)
+        consumer = fluvio.partition_consumer(topic, 0)
         count = 0
         for i in consumer.stream(Offset.beginning()):
             self.assertEqual(
