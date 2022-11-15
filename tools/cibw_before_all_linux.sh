788a37be07e4cdf11aec8984a556faeab02919ea
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-11-15 14:54:53 -0500

    Add zig to aarch gnu build

--- tools/cibw_before_all_linux.sh
+++ tools/cibw_before_all_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e -x
 
-yum install -y openssl-devel
+yum install -y openssl-devel zig
 curl https://sh.rustup.rs --proto '=https' --tlsv1.2 -sSf | sh -s -- --default-toolchain stable -y
