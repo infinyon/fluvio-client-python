19bd114de5181684b2d9ec3b23eaed2d2c758d39
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-11-18 16:50:38 -0500

    Hand wrote a zig installer

--- tools/cibw_before_all_linux.sh
+++ tools/cibw_before_all_linux.sh
@@ -3,3 +3,11 @@ set -e -x
 
 yum install -y openssl-devel zig
 curl https://sh.rustup.rs --proto '=https' --tlsv1.2 -sSf | sh -s -- --default-toolchain stable -y
+
+curl 'https://ziglang.org/download/0.10.0/zig-linux-aarch64-0.10.0.tar.xz' -o ./zig-linux-aarch64-0.10.0.tar.xz
+tar xvf ./zig-linux-aarch64-0.10.0.tar.xz
+ls -lah ./zig-linux-aarch64-0.10.0/zig
+echo $PATH
+mkdir -p $HOME/.bin
+cp ./zig-linux-aarch64-0.10.0/zig $HOME/.bin
+zig --help
