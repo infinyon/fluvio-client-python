f5c927d9557aed841aa066a85535e2f8a7b57131
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-11-21 16:48:20 -0500

    Maybe fixed things

--- tools/cibw_before_all_linux.sh
+++ tools/cibw_before_all_linux.sh
@@ -9,9 +9,9 @@ curl "https://ziglang.org/download/${ZIG_VERSION}/${ZIG_ZIP_FILE}" -o ./${ZIG_ZI
 tar xf ./${ZIG_ZIP_FILE}
 ls -lah ./${ZIG_DIRECTORY}/zig
 echo $PATH
-mkdir -p $HOME/.bin
-cp ./${ZIG_DIRECTORY}/zig $HOME/.bin
-$HOME/.bin/zig --help
+cp ./${ZIG_DIRECTORY}/zig /bin
+zig --help
 
 yum install -y openssl-devel
 curl https://sh.rustup.rs --proto '=https' --tlsv1.2 -sSf | sh -s -- --default-toolchain stable -y
+cp $HOME/.cargo/bin/* /bin/
