fc5b8f8259f15c69b8f402007db366039c968e3b
morenol <22335041+morenol@users.noreply.github.com>
2023-06-28 14:05:20 -0400

    Update test_fluvio_python.py

--- integration-tests/test_fluvio_python.py
+++ integration-tests/test_fluvio_python.py
@@ -14,6 +14,7 @@ def create_topic(topic):
 
 def delete_topic(topic):
     import subprocess
+    time.sleep(1)
 
     subprocess.run(f"fluvio topic delete {topic}", shell=True).check_returncode()
 
@@ -46,7 +47,7 @@ class CommonFluvioSmartModuleTestCase(unittest.TestCase):
         # FIXME: without this the tests fail. Some topics get created but with offset -1
         import time
 
-        time.sleep(1)
+        time.sleep(2)
 
     def setUp(self):
         self.common_setup()
