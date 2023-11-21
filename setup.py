bf48931ac23707a26dcb300861c9186ae2628212
Alan Chen <argot@digikata.com>
2023-11-21 10:53:37 -0800

    bump 0.15.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension, Strip
 
 setup(
     name="fluvio",
-    version="0.15.7",
+    version="0.15.8",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Fluvio Contributors",
