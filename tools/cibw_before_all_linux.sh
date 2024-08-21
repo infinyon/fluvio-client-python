2f8e76fbb150edf23fe07658906ad97c8d20dfc1
Felipe Cardozo <fraifelipe@gmail.com>
2024-08-20 23:56:12 -0300

    fix: publish build wheels

--- tools/cibw_before_all_linux.sh
+++ tools/cibw_before_all_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
 
-yum install -y openssl-devel perl-IPC-Cmd
+yum install -y openssl-devel perl-IPC-Cmd clang-devel
 curl https://sh.rustup.rs --proto '=https' --tlsv1.2 -sSf | sh -s -- --default-toolchain stable -y
 cp $HOME/.cargo/bin/* /bin/
