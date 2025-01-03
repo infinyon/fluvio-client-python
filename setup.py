91df4dd5f8e8adaaa892f61df61b622e806971c0
Felipe Cardozo <fraifelipe@gmail.com>
2025-01-03 17:49:43 -0300

    chore: release v0.18.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="fluvio",
-    version="0.18.0",
+    version="0.18.1",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Fluvio Contributors",
